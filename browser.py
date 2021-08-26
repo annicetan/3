f2aa43faef2b511b6ec5106c8bcdadf93e3d10a1
annicetan <85103627+annicetan@users.noreply.github.com>
2021-08-26 10:47:49 +0800

    Update browser.py

--- browser.py
+++ browser.py
@@ -1,24 +1,31 @@
+from scrapy.http import HtmlResponse
+import grp12
 import requests
-import scrapy
+import unittest
+from unittest import TestCase
+import json
 
-url = 'https://brickset.com/sets/year-2009'
-req = requests.get(url)
-print(req.text)
+session = requests.Session()
 
-print("Status code:")
-print("\t *", req.status_code)
+class test_part5(TestCase):
+    def test_sc(self):
+        response = requests.get(grp12.url)
+        self.assertEqual(response.status_code, 200)
 
-h = requests.head(url)
-print("Header:")
-print("**********")
+    def mua(self):
+        responseua = grp12.modified_ua
+        self.assertEqual(responseua.request.headers, {'User-Agent': 'Mobile', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'})
 
-for line in h.headers:
-    print("\t", line, ":", h.headers[line])
-print("**********")
 
-headers = {
-    'User-Agent': 'Mobile'
-}
-modified_ua = requests.get(url, headers=headers)
-print(modified_ua.request.headers)
-print("**************")
+class test_file(TestCase):
+    def test_json(self):
+        f = open("resultss.json",)
+        data_jpg = json.loads(f.read())
+        self.assertEqual(data_jpg[1], {'Image Link': 'https://images.brickset.com/sets/small/3594-1.jpg?200811140307'})
+
+        f.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
